e0451ea2d8f11d3babfab74728135bbd67b26fa8
UMERZAFAR777 <umerzafar966@gmail.com>
2026-03-18 19:34:24 +0500

    still same

--- umer_portfolio/src/gsap-trial.d.ts
+++ umer_portfolio/src/gsap-trial.d.ts
@@ -1,3 +1,4 @@
 // src/gsap-trial.d.ts
 declare module 'gsap-trial/SplitText';
 declare module 'gsap-trial/Draggable';
+declare module 'gsap-trial/SomeOtherPlugin'; // agar aur plugins use ho rahe hain
